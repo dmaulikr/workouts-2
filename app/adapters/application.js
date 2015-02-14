cf31ad989409473fe2d771b33232001782c5cd50
Joe Sak <joe@joesak.com>
2015-02-13 19:55:31 -0500

    Install cordova

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,6 @@
-import DS from "ember-data";
+import DS from 'ember-data';
+import config from '../config/environment';
 
-export default DS.FixtureAdapter.extend({});
+export default DS.ActiveModelAdapter.extend({
+  host: config.apiUrl
+});
